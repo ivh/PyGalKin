a7893816ea7aa4f0d4805464ef1258763702bd88
thomas marquart <tom@gimli.student.uu.se>
2009-10-05 10:30:16 +0200

    added db.py

---
+++ db.py
@@ -0,0 +1,42 @@
+"""
+db.py
+
+"""
+
+from os.path import exists
+from sqlite3 import dbapi2 as sqlite
+import numpy as N
+from sdss import sqlcl
+
+DBNAME='/data/cigale.db'
+
+def setupdb(dbname=DBNAME):
+    if not exists(dbname): return "Database not there!"
+    connection=sqlite.connect(dbname)
+    cursor=connection.cursor()
+    return connection,cursor
+
+def getg(curs,cols,where='gid NOTNULL',table='galax'):
+    curs.execute("SELECT %s FROM %s WHERE %s"%(cols,table,where))
+    return N.transpose(map(list,curs.fetchall()))
+
+def geto(curs,cols,where='oid NOTNULL',table='obspar'):
+    curs.execute("SELECT %s FROM %s WHERE %s"%(cols,table,where))
+    return N.transpose(map(list,curs.fetchall()))
+
+def getd(curs,cols,where='did NOTNULL',table='data'):
+    curs.execute("SELECT %s FROM %s WHERE %s"%(cols,table,where))
+    return N.transpose(map(list,curs.fetchall()))
+
+def getSDSSids(curs):
+    tol=0.00833
+    ids=getg(curs,'gid')[0]
+    ra,dec=getg(curs,'ra,dec')
+    names=getg(curs,'name')[0]
+    for i,name in enumerate(names):
+        dec0,dec1=dec[i]-tol,dec[i]+tol
+        ra0,ra1=ra[i]-tol,ra[i]+tol
+        if True:
+        #if "UM 501" in name:
+            print i,name,ra[i],dec[i]
+            print sqlcl.sqlcl("SELECT objid,specobjid,ra,dec,z from SpecPhotoAll WHERE (ra BETWEEN %s AND %s) AND (dec BETWEEN %s AND %s) AND (specobjid !=0) AND (primtarget & 0x40 > 0)"%(ra0,ra1,dec0,dec1))
